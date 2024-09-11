e1b11aedc73a69c989226d801c9d3424eb3aafb0
Thabiso <tmosenohi99@gmail.com>
2024-09-11 12:04:26 +0200

    Added more 3 more questions to each quiz + added the history quiz

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -3,6 +3,8 @@ import { createRouter, createWebHistory } from "vue-router";
 import home from '../../views/home.vue';
 import about from '../components/about.vue';
 import notFound from '../components/notFound.vue'
+import quizesView from '../../views/quizePage.vue'
+
 
 const routes = [
 
@@ -14,6 +16,13 @@ const routes = [
         path:'/about',
         component:about
     },
+    ,
+       {path:"/quizes/:id",
+   
+        name:'quizes',
+    
+        component:quizesView
+        },
     {
         path:'/:pathMatch(.*)*',
         component:notFound
